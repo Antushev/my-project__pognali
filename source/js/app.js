9ce2bfd40ccb750abd308f2fba0fa603f2625da0
Dmitry Antushev <fyneitd@mail.ru>
2021-09-05 19:47:44 +0300

    Add styles for index.html

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  let noJsBlock = document.querySelector('.page-header--no-js');
+   let noJsBlock = document.querySelector('.page-header--no-js');
   noJsBlock.classList.remove('page-header--no-js');
 
   let pathOfWhiteLogo = '../img/logo-full.svg';
@@ -17,4 +17,23 @@
       logo.src = pathOfWhiteLogo;
     }
   });
+
+  let buttonOpenModal = document.querySelector('.add-profile__button--open');
+  let buttonCloseModal = document.querySelector('.add-profile__button--close');
+  let modal = document.querySelector('.add-profile__hidden-block');
+
+  buttonOpenModal.addEventListener('click', () => {
+    modal.classList.add('add-profile__hidden-block--open');
+  });
+
+  buttonCloseModal.addEventListener('click', () => {
+    modal.classList.remove('add-profile__hidden-block--open');
+  });
+
+  document.addEventListener('click', (evt) => {
+    if (modal !== evt.target && buttonOpenModal !== evt.target) {
+      modal.classList.remove('add-profile__hidden-block--open');
+    }
+  });
+
 }());
